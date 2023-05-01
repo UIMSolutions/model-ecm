101662809a273c9e852e2671f24ba15b695c04b8
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-01 08:21:09 +0200

    Adding tests

--- source/models/ecm/package.d
+++ source/models/ecm/package.d
@@ -4,6 +4,6 @@ public import models.myname.attributes;
 public import models.myname.classes;
 public import models.myname.elements;
 public import models.myname.entities;
-public import models.myname.interfaces;
+public import models.myname.tests;
 public import models.myname.usecases;
 public import models.myname.values;
