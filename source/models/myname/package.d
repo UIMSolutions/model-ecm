51cf35298caef767b310a4d0de56e15897d5f55f
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-01 08:19:55 +0200

    Renaming namespace

--- source/models/myname/package.d
+++
@@ -1,9 +0,0 @@
-module models.myname;
-
-public import models.myname.attributes;
-public import models.myname.classes;
-public import models.myname.elements;
-public import models.myname.entities;
-public import models.myname.interfaces;
-public import models.myname.usecases;
-public import models.myname.values;
